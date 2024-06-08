b05582c61ea8708b38dd4281f427088b7963266a
Raúl Quimbaya <raulquimbaya4@gmail.com>
2024-06-08 00:25:54 -0500

    Fixing small details

--- src/components/Atom.tsx
+++ src/components/Atom.tsx
@@ -42,10 +42,10 @@ const Orbital = ({ radius, numElectrons, speed, electronSize }: { radius: number
 }
 
 interface Atomo2DProps {
-  symbol: string
-  orbitalSpeeds: number[]
-  orbitalRadii: number[]
-  numElectrons: number[]
+  symbol?: string
+  orbitalSpeeds: number[] | []
+  orbitalRadii: number[] | []
+  numElectrons: number[] | []
 }
 
 const Atomo2D = ({ symbol, orbitalSpeeds, orbitalRadii, numElectrons }: Atomo2DProps) => {
